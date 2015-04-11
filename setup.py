11eb18a22b8c7d7bb7fcb11e221df7a20a8d931b
Danny Goldstein <dgold@berkeley.edu>
2015-04-10 17:39:59 -0700

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='qnd',
+      version='0.1',
+      description='Quick and Dirty (but Pretty Good) Designs',
+      author='Rollin Thomas, Danny Goldstein',
+      author_email='rcthomas@lbl.gov',
+      url='https://github.com/rcthomas/qnd',
+      packages=['qnd'],
+     )
